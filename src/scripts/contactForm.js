aa3d7da2b24bde387ee5f66eef692d300e4eddd8
younsai <younesyounes066@gmail.com>
2024-11-26 21:34:02 +0100

    update contact form

---
+++ src/scripts/contactForm.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById('submit-button')
+btn.onclick = ()=>{
+    alert("Thank you for your message! We've received it and will get back to you as soon as possible.")
+}
